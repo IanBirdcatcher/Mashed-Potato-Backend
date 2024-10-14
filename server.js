6859b3bdb05482f1a09e19f7e36c34f7f9a5738d
Sage Myers <sage.myers@eagles.oc.edu>
2024-10-14 18:33:26 -0500

    deleted the tutorial stuff again

--- server.js
+++ server.js
@@ -4,40 +4,39 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 const db = require("./app/models");
 
+// Sync database
 db.sequelize.sync();
 
+// Configure CORS options
 var corsOptions = {
   origin: "http://localhost:8081",
 };
-
 app.use(cors(corsOptions));
 app.options("*", cors());
 
-// parse requests of content-type - application/json
+// Parse requests with JSON payloads
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
+// Parse requests with URL-encoded payloads
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
+// Basic route for testing server functionality
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to the resume auto-builder application." });
 });
 
+// Import routes
 require("./app/routes/auth.routes.js")(app);
 require("./app/routes/user.routes")(app);
 require("./app/routes/tutorial.routes")(app);
 require("./app/routes/lesson.routes")(app);
 
-// set port, listen for requests
+// Set the server to listen on a specified port
 const PORT = process.env.PORT || 3100;
-if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 
 module.exports = app;
