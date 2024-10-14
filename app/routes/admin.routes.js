2c77a11f6bd4a6eea779bd27f5f7b730c5725fec
Sage Myers <sage.myers@eagles.oc.edu>
2024-10-14 14:12:06 -0500

    added the admin route

--- app/routes/admin.routes.js
+++ app/routes/admin.routes.js
@@ -1,28 +1,13 @@
 module.exports = (app) => {
-  const tutorials = require("../controllers/tutorial.controller.js");
+  const admins = require("../controllers/admin.controller.js");
   const { authenticate } = require("../authorization/authorization.js");
   var router = require("express").Router();
 
-  // Create a new Tutorial
-  router.post("/", [authenticate], tutorials.create);
+  router.post("/", [authenticate], admins.create);
+  router.get("/", [authenticate], admins.findAll);
+  router.get("/:id", [authenticate], admins.findOne);
+  router.put("/:id", [authenticate], admins.update);
+  router.delete("/:id", [authenticate], admins.delete);
 
-  // Retrieve all Tutorials
-  router.get("/", [authenticate], tutorials.findAll);
-
-  // Retrieve all Tutorials for user
-  router.get("/userTut/:userId", [authenticate], tutorials.findAllForUser);
-
-  // Retrieve a single Tutorial with id
-  router.get("/:id", [authenticate], tutorials.findOne);
-
-  // Update a Tutorial with id
-  router.put("/:id", [authenticate], tutorials.update);
-
-  // Delete a Tutorial with id
-  router.delete("/:id", [authenticate], tutorials.delete);
-
-  // Delete all Tutorials
-  router.delete("/", [authenticate], tutorials.deleteAll);
-
-  app.use("/tutorial/tutorials", router);
+  app.use("/admin", router);
 };
