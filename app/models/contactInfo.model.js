009acf0a3b6978ce554be8ac92a6a5751d5c49bb
Sage Myers <sage.myers@eagles.oc.edu>
2024-10-23 10:26:35 -0500

    fixed typos

--- app/models/contactInfo.model.js
+++ app/models/contactInfo.model.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, Sequelize) => {
         allowNull: false,
       },
       address:{
-        type: sequelize.STRING,
+        type: Sequelize.STRING,
         allowNull: false,
       }
     });
