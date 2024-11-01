66477906f60ebf0a966648d1f5182497abc52190
Sage Myers <sage.myers@eagles.oc.edu>
2024-11-01 14:52:44 -0500

    first

--- app/routes/education.routes.js
+++ app/routes/education.routes.js
@@ -3,10 +3,10 @@ module.exports = (app) => {
     const { authenticate } = require("../authorization/authorization.js");
     var router = require("express").Router();
   
-    router.post("/", [authenticate], education.create);
-    router.get("/", [authenticate], education.findAll);
-    router.get("/:id", [authenticate], education.findOne);
-    router.put("/:id", [authenticate], education.update);
+    router.post("/", [authenticate], education.createbyUserId);// you give the user id 
+    router.get("/byUser/:id", [authenticate], education.findAllForUser);// you give the user id 
+    router.get("/:id", [authenticate], education.findOnebyUserId); 
+    router.put("/:id", [authenticate], education.update); // you give the user id and educaton id
     router.delete("/:id", [authenticate], education.delete);
   
     app.use("/education", router);
