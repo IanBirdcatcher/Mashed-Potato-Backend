3de326f4cba07f51053fc9951cac18c231792747
SypnoticVoid <logan.murrie@eagles.oc.edu>
2024-11-18 10:04:22 -0600

    Added new variables to controller and models

--- app/controllers/resume.controller.js
+++ app/controllers/resume.controller.js
@@ -17,7 +17,7 @@ exports.create = (req, res) => {
     resumeId: req.body.resumeId,
     resumeName: req.body.resumeName,
     template: req.body.template,
-    resumeContent: req.body.resumeContent,
+    ProfSummary: req.body.ProfSummary,
     userId: req.body.userId,
   };
   // Save Resume in the database
--- app/models/resume.model.js
+++ app/models/resume.model.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, Sequelize) => {
         type: Sequelize.STRING,
         allowNull: false,
       },
+      ProfSummary: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       template: {
         type: Sequelize.STRING,
         allowNull: false,
