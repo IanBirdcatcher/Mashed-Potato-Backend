86a946dc0d0a0ce46bfbee58010dc805f33be7bb
Logan Murrie <logan.murrie@eagles.oc.edu>
2024-11-18 21:09:42 -0600

    update the User Routes to add Admin Functions

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,11 +3,16 @@ module.exports = (app) => {
   const { authenticate } = require("../authorization/authorization.js");
   var router = require("express").Router();
 
+
+  //user Routes
   router.post("/", [authenticate], user.create);
   router.get("/", [authenticate], user.findAll);
   router.get("/:id", [authenticate], user.findOne);
   router.put("/:id", [authenticate], user.update);
   router.delete("/:id", [authenticate], user.delete);
 
+  //admin routes
+  router.get("/admins", [authenticate], user.findAdmins);
+  router.post("/admins", [authenticate], user.addAdmin);
   app.use("/resume-t2/user", router);
 };
